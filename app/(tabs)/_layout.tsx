201f4d7a8ba192458c1f16502f6151a6d009d09f
Mikhail Smirnov <urine89@mail.ru>
2024-05-21 16:29:32 +0300

    Add icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,12 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function Layout() {
   return (
     <Tabs>
-      <Tabs.Screen name='profile' options={{ headerTitle: 'Profile' }} />
-      <Tabs.Screen name='checkin' options={{ headerTitle: 'Check In' }} />
+      <Tabs.Screen name='profile' options={{ headerTitle: 'Profile', tabBarIcon: () => <Ionicons name="person" size={25} /> }} />
+      <Tabs.Screen name='checkin' options={{ headerTitle: 'Check In', tabBarIcon: () => <Ionicons name="checkmark-circle" size={25} /> }} />
     </Tabs>
   );
 }
